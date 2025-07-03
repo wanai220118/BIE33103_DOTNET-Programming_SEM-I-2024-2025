25871ea5d0cf31f2b1ce30c093126bcec6c691b8
wanai220118 <wanadhani123@gmail.com>
2025-07-03 20:07:19 +0800

    add files related

---
+++ LAB_2/Lab_2/Lab_2/BMI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lab2
+{
+    class BMI
+    {
+        static void Main(string[] args)
+        {
+            double bmi, w, h;
+
+            Console.Write("Enter your weight (in kg): ");
+            w = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your height (in m): ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            bmi = w / (h * h);
+            Console.WriteLine("Your BMI is: " + bmi);
+
+            int category;
+
+            if (bmi < 18.5)
+                category = 1; // Underweight
+            else if (bmi >= 18.5 && bmi < 24.9)
+                category = 2; // Healthy
+            else if (bmi >= 24.9 && bmi < 30)
+                category = 3; // Overweight
+            else
+                category = 4; // Obesity
+
+            switch (category)
+            {
+                case 1:
+                    Console.WriteLine("\nUnderweight");
+                    break;
+                case 2:
+                    Console.WriteLine("\nHealthy");
+                    break;
+                case 3:
+                    Console.WriteLine("\nOverweight");
+                    break;
+                case 4:
+                    Console.WriteLine("\nSuffering from Obesity");
+                    break;
+            }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey(); // Pause
+        }
+    }
+}
